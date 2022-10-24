c04caf7b590717edbc7e3243d97fddc246179439
Felipe Lustosa <felipelustosaa6@gmail.com>
2022-10-23 21:08:58 -0300

    indo para o sanity.io

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -6,12 +7,12 @@ const Projects = (props: Props) => {
     const projects = [1, 2, 3, 4, 5];
 
     return (
-        <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1.5 }} className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Projects</h3>
-            <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+            <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20  scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
                 {projects.map((project, i) => (
                     <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fugross.gallerycdn.vsassets.io%2Fextensions%2Fugross%2Fvscode-react-snippets%2F1.3.0%2F1519481679046%2FMicrosoft.VisualStudio.Services.Icons.Default&f=1&nofb=1&ipt=c10d099fa70fbd6d1d56d5aaf41fcf63a1d3424dac8cf7d3d3e08e83afe95fe4&ipo=images" />
+                        <motion.img initial={{ y: -300, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ duration: 1.2 }} viewport={{ once: true }} className="h-64 max-w-96 object-cover" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs.tmimgcdn.com%2Fscr%2F45000%2F45042_01_home_original_1200.jpg%3Fwidth%3D1000&f=1&nofb=1&ipt=2fa34e57f637df6ef2017f1fbf0aae18ad71fb2b4c5a96d3852cee34619f5a77&ipo=images" />
                         <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                             <h4 className="text-4xl font-semibold text-center">
                                 <span className="underline decoration-[#F7AB0A]/50">
@@ -19,12 +20,13 @@ const Projects = (props: Props) => {
                                 </span>{" "}
                                 UPS clone
                             </h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
                         </div>
                     </div>
                 ))}
             </div>
             <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12 "></div>
-        </div>
+        </motion.div>
     );
 };
 
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,9 +7,10 @@ type Props = {
 
 const Skill = ({ directionLeft }: Props) => {
     return (
-        <div className="group relative flex cursor-pointer">
-            <motion.img initial={{ x: directionLeft ? -200 : 200, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 1 }} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fugross.gallerycdn.vsassets.io%2Fextensions%2Fugross%2Fvscode-react-snippets%2F1.3.0%2F1519481679046%2FMicrosoft.VisualStudio.Services.Icons.Default&f=1&nofb=1&ipt=c10d099fa70fbd6d1d56d5aaf41fcf63a1d3424dac8cf7d3d3e08e83afe95fe4&ipo=images" className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 easy-in-out" />
-            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full z-0">
+        <div className="group relative flex cursor-pointer overflow-hidden">
+            {/* <motion.img initial={{ x: directionLeft ? -200 : 200, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ duration: 1 }} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fugross.gallerycdn.vsassets.io%2Fextensions%2Fugross%2Fvscode-react-snippets%2F1.3.0%2F1519481679046%2FMicrosoft.VisualStudio.Services.Icons.Default&f=1&nofb=1&ipt=c10d099fa70fbd6d1d56d5aaf41fcf63a1d3424dac8cf7d3d3e08e83afe95fe4&ipo=images" className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 easy-in-out" /> */}
+            <motion.img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fugross.gallerycdn.vsassets.io%2Fextensions%2Fugross%2Fvscode-react-snippets%2F1.3.0%2F1519481679046%2FMicrosoft.VisualStudio.Services.Icons.Default&f=1&nofb=1&ipt=c10d099fa70fbd6d1d56d5aaf41fcf63a1d3424dac8cf7d3d3e08e83afe95fe4&ipo=images" className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 easy-in-out" />
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
                 <div className="flex items-center justify-center h-full">
                     <p className="text-3xl font-bold text-black opacity-100">100%</p>
                 </div>
